9140cc3171cb92e74923c9c8ab917d5c483a7362
Jonathan <jonathan.lam.ch@gmail.com>
2024-12-10 16:20:46 -0500

    clean code

--- src/@types/hebrew-date.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'hebrew-date' {
-    export default function hebrewDate(year: number, month: number, day: number): {
-      year: number;
-      month: number;
-      date: number;
-      month_name: string;
-    };
-  }
